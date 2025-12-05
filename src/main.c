478bf82ef84caba3a9322c9cf7bdf0f535356752
jiku0730 <kei2003730@gmail.com>
2025-12-05 13:32:00 +0900

    fix:typo

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ typedef struct hash_table
 	int		value;
 }			hash_table;
 
-hash_table *liner_serch(hash_table *hash_table, size_t size, const char *key)
+hash_table *linear_search(hash_table *hash_table, size_t size, const char *key)
 {
 	size_t	idx;
 
@@ -23,7 +23,7 @@ int main(int argc, char const *argv[])
 {
 	hash_table	ht[] = {{"apple", 3}, {"orange", 10}, {"banana", 5}};
 	size_t		size = sizeof(ht) / sizeof(ht[0]);
-	hash_table	*find = liner_serch(ht, size, "orange");
+	hash_table	*find = linear_search(ht, size, "orange");
 
 	(void)argc;
 	(void)argv;
