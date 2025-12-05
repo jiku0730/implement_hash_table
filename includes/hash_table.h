47898b988026021312514f30c218116f6a4c8444
jiku0730 <kei2003730@gmail.com>
2025-12-05 13:29:56 +0900

    implement: liner serch hashtable

--- includes/hash_table.h
+++ includes/hash_table.h
@@ -4,5 +4,6 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
 #include "hash_table.h"
 
+typedef struct hash_table
+{
+	char 	*key;
+	int		value;
+}			hash_table;
+
+hash_table *liner_serch(hash_table *hash_table, size_t size, const char *key)
+{
+	size_t	idx;
+
+	idx = 0;
+	while (idx < size)
+	{
+		if (strcmp(hash_table[idx].key, key) == 0)
+			return (&hash_table[idx]);
+		idx++;
+	}
+	return (NULL);
+}
 int main(int argc, char const *argv[])
 {
+	hash_table	ht[] = {{"apple", 3}, {"orange", 10}, {"banana", 5}};
+	size_t		size = sizeof(ht) / sizeof(ht[0]);
+	hash_table	*find = liner_serch(ht, size, "orange");
+
 	(void)argc;
 	(void)argv;
-	printf("Hello World!\n");
+	if (!find)
+		return (1);
+	printf("find->key = %s, find->value = %d\n", find->key, find->value);
 	return 0;
 }
